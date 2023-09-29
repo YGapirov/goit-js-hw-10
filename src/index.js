9965549a451839a213be0255dda12e2c4150302b
Yurii <gapirov21@gmail.com>
2023-09-29 12:22:41 +0300

    task 90% done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,95 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
-import './style.css';
 
-   const breedSelect = document.querySelector('.breed-select');
-   const loader = document.querySelector('.loader');
-   const error = document.querySelector('.error');
-   const catInfo = document.querySelector('.cat-info');
-
-   loader.classList.replace('loader', 'is-hidden');
-   error.classList.add('is-hidden');
-   catInfo.classList.add('is-hidden');
+import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select'
 
 
+const elements = {
+  breedSelect: document.querySelector('.breed-select'),
+  loader: document.querySelector('.loader'),
+  errorInfo: document.querySelector('.error'),
+  catInfo: document.querySelector('.cat-info')
+};
+//ініцалізація
+init();
 
-// div.cat-info з'являється зображення і розгорнута інформація про кота: назва породи, опис і темперамент.
-// image:
-// name: 
-// description:
-// temperament:
+function init() {
+   
+   let breedsData;
+   //отримання списку порід і обробка результату
+   showCustomLoader();
+    fetchBreeds()
+    .then(data => {
+        breedsData = data;
+        data.forEach(breed => {
+            const option = document.createElement('option');
+            option.value = breed.id;
+            option.textContent = breed.name;
+           elements.breedSelect.appendChild(option);
+           
+         // Показуємо випадаючий список і приховуємо завантажувач
+            elements.breedSelect.classList.remove('is-hidden');
+            elements.loader.classList.add('is-hidden');
+        })
+   //     new SlimSelect({
+   //      select: elements.breedSelect,
+   //      data: breedsData
+   //  });
+    })
+    .catch(error => {
+       console.log(error);
+       // повідомлення про помилку і приховуємо випадаючий список та завантажувач
+        elements.errorInfo.classList.remove('is-hidden');
+        elements.breedSelect.classList.add('is-hidden');
+        elements.loader.classList.add('is-hidden');
+    })
+
+   
+   //значення випадаючого вікна і приховування лодера інфо і помилок
+    elements.breedSelect.addEventListener('change', () => {
+       const selectBreedId = elements.breedSelect.value;
+       showCustomLoader();
+        elements.loader.classList.remove('is-hidden');
+        elements.catInfo.classList.add('is-hidden');
+         elements.errorInfo.classList.add('is-hidden');
+      //  Notiflix.Notify.failure('❌Oops! Something went wrong! Try reloading the page!', { position: 'center-top', distance: '200px'})
+
+       
+       // Запит на отримання інформації про кота та обробка результату
+        fetchCatByBreed(selectBreedId)
+        .then(result => {
+            const catData = result[0];
+            const breedData = breedsData.find(breed => breed.id === catData.breeds[0].id);
+
+            const markup = createMarkup(catData, breedData);
+            elements.catInfo.innerHTML = markup;
+
+            elements.loader.classList.add('is-hidden');
+            elements.catInfo.classList.remove('is-hidden');
+        })
+        .catch(error => {
+            console.log(error);
+            // elements.errorInfo.classList.remove('is-hidden');
+           elements.loader.classList.add('is-hidden');
+           Notiflix.Notify.failure('❌Oops! Something went wrong! Try reloading the page!', { position: 'center-top', distance: '200px'})
+        })
+    });
+}
+
+function createMarkup(catData, breedData){
+    return `<div class='main-info'> <img src='${catData.url}' width='300' alt='${breedData.name}'/>
+    <h1>${breedData.name}</h1>
+    <p>${breedData.description}</p>
+    <h2>Temperament</h2>
+    <p>${breedData.temperament}</p></div>`
+}
+
+
+//custom-loader
+function showCustomLoader() {
+  const customLoader = document.querySelector('.custom-loader');
+  if (customLoader) {
+    customLoader.style.display = 'flex';
+  }
+}
 
